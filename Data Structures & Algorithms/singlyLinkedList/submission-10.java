0e4ced2fbf9af42f8d358905ca1207f4b5c55b4a
Gervásio Artur <90800218+gervasioartur@users.noreply.github.com>
2026-05-27 15:36:03 +0200

    Add: singlyLinkedList - submission-10

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-10.java
@@ -0,0 +1,79 @@
+class Node {
+        private int data;
+        private Node next;
+
+        Node(int value){
+            data = value;
+            next = null;
+        }
+
+        public Node getNext(){
+            return next;
+        }
+
+        public void setNext(Node value){
+            next = value;
+        }
+
+        public int getData(){
+            return data;
+        }
+    }
+
+class LinkedList {
+    private Node head;
+
+    public LinkedList() {
+        head =  new Node(0);
+    }
+
+    public int get(int index) {
+        int count = 0;
+        Node current = head.getNext();
+        while(current != null){
+            if(index == count) return current.getData();
+            count++;
+            current = current.getNext();
+        }
+        return -1;
+    }
+
+    public void insertHead(int val) {
+        Node newNode = new Node(val);
+        newNode.setNext(head.getNext());
+        head.setNext(newNode);
+    }
+
+    public void insertTail(int val) {
+        Node current = head;
+        Node newTail = new Node(val);
+        while(current.getNext() != null){
+            current = current.getNext();
+        }
+        current.setNext(newTail);
+    }
+
+    public boolean remove(int index) {
+        int count = 0;
+        Node current = head;
+        while(current != null && current.getNext() != null){
+            if(count == index) {
+                current.setNext(current.getNext().getNext());
+                return true;
+            }
+            current = current.getNext();
+            count++;
+        }
+        return false;
+    }
+
+    public ArrayList<Integer> getValues() {
+        ArrayList<Integer> result = new ArrayList<>();
+        Node current = head.getNext();
+        while(current != null ){
+            result.add(current.getData());
+            current = current.getNext();
+        }
+        return result;
+    }
+}
